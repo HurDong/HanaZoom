38356c37b6f48cd0f67b48e33e34d884311544ca
HurDong <gjehdals456@gmail.com>
2025-06-08 04:06:52 +0900

    feat: Spring Boot BE 프로젝트 초기 세팅 및 infra 브랜치 구조 생성 (#1)

---
+++ BE/HanaZoom/src/main/java/com/hanazoom/HanaZoomApplication.java
@@ -0,0 +1,13 @@
+package com.hanazoom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanaZoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanaZoomApplication.class, args);
+	}
+
+}
---
+++ BE/HanaZoom/src/test/java/com/hanazoom/HanaZoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanazoom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanaZoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
