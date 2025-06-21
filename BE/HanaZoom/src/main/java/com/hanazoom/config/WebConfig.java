4aa1759d52befa098b2ccac322b90b916df04cc3
HurDong <gjehdals456@gmail.com>
2025-06-22 02:30:32 +0900

    chore: Docker 및 Gradle 빌드 설정 최적화

--- BE/HanaZoom/src/main/java/com/hanazoom/config/WebConfig.java
+++ BE/HanaZoom/src/main/java/com/hanazoom/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000") // 로컬 개발 환경
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
